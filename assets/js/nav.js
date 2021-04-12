6382137bf6c9a967992bcaf3380b44ab53381ab7
ArthurDhx <arthurdhxpro@gmail.com>
2021-04-12 02:24:17 +0200

    hover les icones de la nav

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,20 +1,24 @@
 
 // fonction qui change la loupe en barre
 const showSearch = document.getElementById('show-search');
-showSearch.addEventListener("click", () => {
-    const searchLoop = document.getElementById('searchLoop');
-    searchLoop.classList.toggle('fa-search');
-    searchLoop.classList.toggle('fa-times');
-})
+if (showSearch != null) {
+    showSearch.addEventListener("click", () => {
+        const searchLoop = document.getElementById('searchLoop');
+        searchLoop.classList.toggle('fa-search');
+        searchLoop.classList.toggle('fa-times');
+    })
 
+}
 // fonction qui change le burger en croix et qui cache les éléments de la nav
 const burger = document.getElementById('burger');
-burger.addEventListener("click", () => {
-    burger.classList.toggle("change");
-    const wrapper = document.getElementById("wrapper");
-    const content = document.getElementById("content");
-    const links = document.getElementById("links");
-    content.classList.toggle("hideNav");
-    links.classList.toggle("hideNav");
-    wrapper.classList.toggle("fixed");
-})
+if (burger != null) {
+    burger.addEventListener("click", () => {
+        burger.classList.toggle("change");
+        const wrapper = document.getElementById("wrapper");
+        const content = document.getElementById("content");
+        const links = document.getElementById("links");
+        content.classList.toggle("hideNav");
+        links.classList.toggle("hideNav");
+        wrapper.classList.toggle("fixed");
+    })
+}
