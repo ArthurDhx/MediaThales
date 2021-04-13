61f51d5dc7dc78007f91879749474bc4cb5cfe8a
ArthurDhx <arthurdhxpro@gmail.com>
2021-04-13 20:24:56 +0200

    :books:

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,12 +1,20 @@
 
 // fonction qui change la loupe en barre
+// et qui affiche la champ d'input
 const searchLoop = document.getElementById('searchLoop');
 if (searchLoop != null) {
     searchLoop.addEventListener("click", () => {
+        // on récupère l'icône loop
         const loop = document.getElementById('loop');
+
+        // on on/off la loupe ou la croix
         loop.classList.toggle('fa-search');
         loop.classList.toggle('fa-times');
+
+        // on récupère le champ d'input
         const formSearch = document.getElementById('form-search');
+
+        // on on/off son affichage
         formSearch.classList.toggle('show-input-search');
     })
 
@@ -15,10 +23,14 @@ if (searchLoop != null) {
 const burger = document.getElementById('burger');
 if (burger != null) {
     burger.addEventListener("click", () => {
+        // on on/off le burger en croix
         burger.classList.toggle("change");
+
         const wrapper = document.getElementById("wrapper");
         const content = document.getElementById("content");
         const links = document.getElementById("links");
+        
+        // on cache l'affichage normal
         content.classList.toggle("hideNav");
         links.classList.toggle("hideNav");
         wrapper.classList.toggle("fixed");
